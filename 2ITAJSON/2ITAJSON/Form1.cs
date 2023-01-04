663528449d0e357ea613a86770b4f14c2cbe8c92
Matav <verner.matej@gmail.com>
2023-01-04 14:26:32 +0100

    2ITAJSON

--- 2ITAJSON/2ITAJSON/Form1.cs
+++ 2ITAJSON/2ITAJSON/Form1.cs
@@ -1,19 +1,57 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace _2ITAJSON
 {
     public partial class Form1 : Form
     {
+        List<Zbran> zbrane = new List<Zbran>();
         public Form1()
         {
             InitializeComponent();
-            VytvorZbran(30, 30, 35, "AK-47", TypZbrane.AR);
+            NactiZeSouboru();
+            if (zbrane.Count == 0)
+            {
+                VytvorZbran(30, 30, 35, "AK-47", TypZbrane.AR);
+                VytvorZbran(100, 100, 15, "Negev", TypZbrane.LMG);
+            }
+            else
+            {
+                VytvorGrafiky();
+            }
+        }
 
+        public void VytvorGrafiky()
+        {
+            for (int i = 0; i < zbrane.Count; i++)
+            {
+                Label label = new Label();
+                zbrane[i].SetLabel(label);
+                tableLayoutPanel1.Controls.Add(label);
+            }
         }
         public void VytvorZbran(int kapacitaNaboju, int aktualniNaboje, int poskozeni, string jmenoZbrane, TypZbrane typZbrane)
         {
             Label label = new Label();
             Zbran zbran = new Zbran(kapacitaNaboju,aktualniNaboje,poskozeni,jmenoZbrane,typZbrane, label);
             tableLayoutPanel1.Controls.Add(label);
+            zbrane.Add(zbran);
+
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string json = JsonSerializer.Serialize(zbrane );
+            MessageBox.Show(json);
+            File.WriteAllText("zbrane.json", json);
+        }
+        private void NactiZeSouboru()
+        {
+            if (!File.Exists("zbrane.json"))
+                return;
+            string savedJson = File.ReadAllText("zbrane.json");
+            zbrane = JsonSerializer.Deserialize<List<Zbran>>(savedJson);
+        }
     }
 }
--- 2ITAJSON/2ITAJSON/Zbran.cs
+++ 2ITAJSON/2ITAJSON/Zbran.cs
@@ -8,13 +8,26 @@ namespace _2ITAJSON
 {
     internal class Zbran
     {
-        public int kapacitaNaboju;
-        public int aktualniNaboje;
-        public int poskozeni;
-        public string jmenoZbrane;
-        public TypZbrane typZbrane;
-        private Label label;
+        private int kapacitaNaboju;
+        public int KapacitaNaboju => kapacitaNaboju;
 
+        private int aktualniNaboje;
+
+        public int AktualniNaboje => aktualniNaboje;
+
+        private int poskozeni;
+        public int Poskozeni => poskozeni;
+        private string jmenoZbrane;
+        public string JmenoZbrane => jmenoZbrane;
+
+        private TypZbrane typZbrane;
+        public TypZbrane TypZbrane => typZbrane;
+        private Label label;
+        public Zbran()
+        {
+
+
+        }
         public Zbran(int kapacitaNaboju, int aktualniNaboje, int poskozeni, string jmenoZbrane, TypZbrane typZbrane, Label label)
         {
             this.kapacitaNaboju = kapacitaNaboju;
@@ -29,6 +42,13 @@ namespace _2ITAJSON
         public void UpdateLabel()
         {
             label.Text = $"{jmenoZbrane} \n Kapacita: {aktualniNaboje}\\{kapacitaNaboju} \n Poškození: {poskozeni} \n Typ: {typZbrane}";
+        }
+
+        public void SetLabel(Label label)
+        {
+            this.label = label;
+            this.label.AutoSize = true;
+            UpdateLabel();
         }
     }
     public enum TypZbrane
